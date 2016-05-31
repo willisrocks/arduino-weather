6d91d83c90a4c3153d0ff7537ded084307094b20
Chris Fenton <cfenton30@gmail.com>
2016-05-31 10:43:58 -0700

    Added arduino code and libraries

--- arduino/weather-pod-arduino/weather-pod-arduino.ino
+++ arduino/weather-pod-arduino/weather-pod-arduino.ino
@@ -1,37 +1,75 @@
+#include <SPI.h>
+#include <Dhcp.h>
+#include <Dns.h>
+#include <HttpClient.h>
+#include <Ethernet.h>
+#include <EthernetClient.h>
+#include <EthernetServer.h>
+#include <EthernetUdp.h>
+#include <SD.h>
+#include <Wire.h>
+#include <Adafruit_MPL3115A2.h>
+
+Adafruit_MPL3115A2 baro = Adafruit_MPL3115A2();
+
+// **********************************
+// Variables
+// **********************************
+
 // Pins
 const int buttonPin = 2;     // the number of the pushbutton pin
 const int ledPin =  3;      // the number of the LED pin
+const int chipSelect = 4;
 
-// Variables
+// Buttons
 int buttonState;         // variable for reading the pushbutton status
 int ledState = HIGH;
 int lastButtonState = LOW;
 long lastDebounceTime = 0;
 long debounceDelay = 50;
 
+// Weather
+Adafruit_MPL3115A2 baro = Adafruit_MPL3115A2();
+byte mac[] = { 0xFE, 0xAD, 0xBE, 0xEF, 0xFE, 0xDE };
+const char kHostname[] = "httpbin.org";
+const char kPath[] = "/post?ross=noob";
+const int kNetworkTimeout = 30 * 1000;
+const int kNetworkDelay = 1000;
+int networkSetup = 0;
+int sdcardSetup = 0;
+
+
 void setup() {
-  // initialize the LED pin as an output:
+  // buttons
   pinMode(ledPin, OUTPUT);
-  
-  // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);
-
-  // start serial port
-  Serial.begin(9600);
-
-  // set initial LED state
   digitalWrite(ledPin, ledState);
+
+  Serial.begin(9600);
 }
 
 void loop() {
   if (ledState == HIGH){
     Serial.println("Gathering Data");
-    // INSERT DATA GATHERING CODE
+    if (sdcardSetup == 1) {
+      // INSERT DATA GATHERING CODE
+
+      
+    } else {
+      setupSD;
+    }
 
   }
   if (ledState == LOW){
     Serial.println("Pushing Data");
-    // INSERT DATA UPLOAD CODE
+    
+    if (networkSetup == 1) {
+      // INSERT DATA UPLOAD CODE
+
+      
+    } else {
+      setupNetwork();
+    }
 
   }
 
@@ -67,3 +105,42 @@ void loop() {
   lastButtonState = reading;
 
 }
+
+void setupNetwork() {
+  // network and SD card
+  Serial.println("Starting w5100...");
+  if (!Ethernet.begin(mac)) {
+    
+    Serial.println("Ethernet not connected");
+    
+  } else {
+    
+    Serial.println(Ethernet.localIP());
+
+    while (Ethernet.begin(mac) != 1)
+    {
+      Serial.println("Error getting IP address via DHCP, trying again...");
+      delay(10000);
+    }
+  }
+  Serial.println("Ethernet connected");
+  networkSetup = 1;
+}
+
+void setupSD() {
+  while (!Serial) {
+      ; // wait for serial port to connect. Needed for native USB port only
+    }
+
+    Serial.print("Initializing SD card...");
+
+    // see if the card is present and can be initialized:
+    if (!SD.begin(chipSelect)) {
+      Serial.println("Card failed, or not present");
+      // don't do anything more:
+      return;
+    }
+    Serial.println("card initialized.");
+    sdcardSetup = 1;
+}
+
